b09355e8a950b464ee5968cb17fe00b2fc3e6dc4
Oleg Rumiancev <oleg.rumiancev@gmail.com>
2018-07-19 22:16:44 -0400

    added handling of json files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 let gulp = require('gulp');
 
-gulp.task('cssCopy', function(){
-  return gulp.src('src/**/*.css')
-    .pipe( gulp.dest('lib/') );
+gulp.task('copyassets', function(){
+  return gulp.src(['src/**/*.css', 'src/**/*.json'])
+    .pipe(gulp.dest('lib/') );
 });
